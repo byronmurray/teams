67a4ef7e5225bddf4ca07f8dc77baff4e6b0e391
vagrant <vagrant@homestead>
2017-02-21 04:12:25 +0000

    added comments

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Comment;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,44 @@
         <h2 class="blog-post-title"><a href="posts/{{ $post->id }}">{{ $post->title }}</a></h2>
         <p class="blog-post-meta">January 1, 2014 by Mark</p>
         <p>{{ $post->body }}</p>
+
+        <hr>
+
+        <div class="comments">
+
+	        <div class="list-group">
+	        	
+				@foreach ($post->comments as $comment)
+					
+					<li class="list-group-item">
+
+					<strong>
+						{{ $comment->created_at->diffForHumans() }} : &nbsp;
+					</strong>
+						
+						{{ $comment->body }}
+
+					</li>
+						
+				@endforeach
+
+			</div>
+
+        </div>
+
+        <form action="/posts/{{ $post->id }}/comments" method="POST">
+	    {{ csrf_field() }}
+
+		  <div class="form-group">
+		    <textarea type="text" id="body" name="body" class="form-control" placeholder="Post a comment here....."></textarea>
+		  </div>
+
+		  <div class="form-group">
+		  	<button type="submit" class="btn btn-primary">Add Comment</button>
+		  </div>
+		</form>
+
+
     </div><!-- /.blog-post -->       
 
 
